5bea5cc549be33a23cf8f1bfaf02074047055351
Chris McLaren <csmclaren@me.com>
2024-07-06 13:19:33 -0400

    Collapse the "Top" node in the generated Texinfo file. Closes #30

--- tools/pandoc/filter-adjust-header-depths.lua
+++ tools/pandoc/filter-adjust-header-depths.lua
@@ -1,7 +1,13 @@
 function Header(elem)
-  if elem.level >= 5 then
-    elem.level = 4
-  end
+  if elem.level == 1 then
+    return {}
+  else
+    if elem.level >= 5 then
+      elem.level = 4
+    else
+      elem.level = elem.level - 1
+    end
 
-  return elem
+    return elem
+  end
 end
